7fc427c550ccdf685c9d21f2a9c46f64aca6bdaf
DAVI CASTILHO LOPES <davi.lopes@a.ucb.br>
2024-03-13 10:01:37 -0300

    Atividade 1 adiconada

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+const x = 2;
+const y = 3;
+let z = 0;
+
+z = x + y;
+console.log("Resultado da soma: "+ z +" entre "+ x +" e "+ y);
+z = x - y;
+console.log(`Resultado da sub: ${z} entre ${x} e ${y}`);
+z = x * y;
+console.log(`Resultado da mult: ${z} entre ${x} e ${y}`);
+z = x / y;
+console.log(`Resultado da div: ${z} entre ${x} e ${y}`);
