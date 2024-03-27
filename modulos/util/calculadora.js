38fb1fcc7e606781d00be242264390b8abb60420
Davi Castilho <davicastilho040@gmail.com>
2024-03-27 11:07:48 -0300

    add modulos

---
+++ modulos/util/calculadora.js
@@ -0,0 +1,25 @@
+function somar(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function multi(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+const PI = 3.14;
+
+module.exports = {
+    somar,
+    sub,
+    multi,
+    div,
+    PI
+}
